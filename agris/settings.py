80fca2a4dea1d4369ccc02f561bccb624f085667
M Alif <itsal50505@gmail.com>
2024-11-28 15:34:31 +0600

    somechanges

--- agris/settings.py
+++ agris/settings.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = [*]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
